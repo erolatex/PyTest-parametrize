6f56e54b5bc1eafb03dd7dcdccf9e1a80e6daff2
Maxim Rabinovich <41438388+erolatex@users.noreply.github.com>
2019-06-16 03:47:06 +0300

    Comment sleeping

--- test_item_card.py
+++ test_item_card.py
@@ -4,7 +4,7 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_order_button_presence(browser):
     browser.get(link)
-    time.sleep(30)
+    #time.sleep(30)
     browser.find_element_by_css_selector(".btn-add-to-basket")
 
 
