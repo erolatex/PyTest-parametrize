ec6e8167359b79ea12ee53677bed2153e6ddd868
Maxim Rabinovich <41438388+erolatex@users.noreply.github.com>
2019-06-16 09:29:35 +0300

    Add basket button assertion

--- test_item_card.py
+++ test_item_card.py
@@ -1,10 +1,11 @@
 import time
+from selenium.webdriver.support import expected_conditions as EC
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_order_button_presence(browser):
     browser.get(link)
     #time.sleep(30)
-    browser.find_element_by_css_selector(".btn-add-to-basket")
-
-
+   
+    basket_button = browser.find_element_by_css_selector(".btn-add-to-basket")
+    assert EC.visibility_of(basket_button), "Basket button isn't visible or missing"
